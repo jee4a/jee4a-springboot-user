b6930dd87d250db4ed66c5fbc555978d075d2672
tpeng <39822836@qq.com>
2018-01-29 11:23:11 +0800

    断熔器

--- src/main/java/com/jee4a/user/controller/UserController.java
+++ src/main/java/com/jee4a/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.jee4a.user.controller;
 
+import java.util.Random;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -32,6 +34,23 @@ public class UserController {
 		@SuppressWarnings("deprecation")
 		ServiceInstance instance  = discoveryClient.getLocalServiceInstance() ;
 		logger.info("/user/{},host:{},service_id:{}",id,instance.getHost(),instance.getServiceId());
+		return userService.queryById(id) ;
+	}
+	
+	/**
+	 * 
+	 * 描述     :熔断器示例 ，模拟服务阻塞 
+	 * @author tpeng 2018年1月29日
+	 * @email 398222836@qq.com
+	 */
+	@RequestMapping(value="/user-1/{id}")
+	public User queryByUserId1(@PathVariable Integer id) throws InterruptedException {
+		@SuppressWarnings("deprecation")
+		ServiceInstance instance  = discoveryClient.getLocalServiceInstance() ;
+		int sleepTime = new Random().nextInt(3000) ;
+		Thread.sleep(sleepTime);
+		logger.info("sleep time : {}" ,sleepTime );
+		logger.info("/user-1/{},host:{},service_id:{}",id,instance.getHost(),instance.getServiceId());
 		return userService.queryById(id) ;
 	}
 }
--- src/main/java/com/jee4a/user/service/UserService.java
+++ src/main/java/com/jee4a/user/service/UserService.java
@@ -1,7 +1,12 @@
 package com.jee4a.user.service;
 
+import java.util.Random;
+
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.stereotype.Service;
 
 import com.jee4a.common.exceptions.ApiException;
@@ -16,9 +21,17 @@ import com.jee4a.user.manager.UserManager;
 @Service
 public class UserService {
 
+	private final static Logger  logger = LoggerFactory.getLogger(UserService.class) ;
+	
 	@Resource
 	private UserManager userManger ;
 	
+	/**
+	 * 
+	 * 描述     : 
+	 * @author tpeng 2018年1月29日
+	 * @email 398222836@qq.com
+	 */
 	public User queryById(Integer id) {
 		if(id==null || id<0) {
 			throw new ApiException(-10000,"id error") ;
@@ -26,4 +39,7 @@ public class UserService {
 		
 		return userManger.selectByPrimaryKey(id) ;
 	}
+	
+	 
+	
 }
